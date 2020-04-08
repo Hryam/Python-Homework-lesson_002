c06607a941b2eee7b62b7461596f2f5bdff2d457
Nikolay Ahremenko <holderchip@gmail.com>
2020-04-08 22:22:52 +0700

    Решил первую задачу

--- 00_distance.py
+++ 00_distance.py
@@ -15,7 +15,10 @@ sites = {
 distances = {}
 
 # TODO здесь заполнение словаря
-
+Moscow_London = ((sites['Moscow'][0]-sites['London'][0])**2+(sites['Moscow'][1]-sites['London'][1])**2)**.5 
+London_Paris = ((sites['London'][0]-sites['Paris'][0])**2+(sites['London'][1]-sites['Paris'][1])**2)**.5
+Moscow_Paris = ((sites['Moscow'][0]-sites['Paris'][0])**2+(sites['Moscow'][1]-sites['Paris'][1])**2)**.5
+distances  = {Moscow_London, London_Paris, Moscow_Paris,}
 print(distances)
 
 
